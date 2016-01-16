f0f5671a51c1c1150a60cce3205acfd490787a81
chenglong zou <chenglong.zq@gmail.com>
2016-01-16 19:48:30 +0100

    Moving client nexioChat to client.js

--- public/client.js
+++ public/client.js
@@ -1,5 +1,3 @@
-
-
 // alert("Here goes the client script");
 console.log("Client socket io handler api"); // each of the client will conect to the node server and register its identifier
 // (this identifier should be unique among all the users and also has to be persistent)
@@ -8,21 +6,68 @@ console.log("Client socket io handler api"); // each of the client will conect t
 // socket code
 // load socket.io-client
 /*
-var socket = io(); // its default is to serve from the host page.
-*/
-
-
-
-// register actions on event
-/**
-    {
-    onMessage : refreshDiv
-    }
+ var socket = io(); // its default is to serve from the host page.
  */
-var register = function(actions){
-
-    //
 
 
+// add listener on Enter key to send message (emit whatever)
 
-};
+/**
+ * Utils:
+ * retrieve get perameters
+ */
+function getSearchParameters() {
+    var prmstr = window.location.search.substr(1);
+    return prmstr != null && prmstr != "" ? transformToAssocArray(prmstr) : {};
+}
+function transformToAssocArray(prmstr) {
+    var params = {};
+    var prmarr = prmstr.split("&");
+    for (var i = 0; i < prmarr.length; i++) {
+        var tmparr = prmarr[i].split("=");
+        params[tmparr[0]] = tmparr[1];
+    }
+    return params;
+}
+
+
+/**
+ * this will be the content of client.js
+ * url is the location of the
+ */
+function nexioChat(options) {
+
+    nexioChat.prototype.socket = io();
+
+    // usuario se registra en una incidencia del servidor
+    nexioChat.prototype.join = function (incidents) {
+        incidents.forEach(function (incident) {
+            nexioChat.prototype.socket.emit('join', incident); // the client join some incident room
+        });
+    };
+
+    // usuario envia una notificación al servidor, para forwarding
+    nexioChat.prototype.notify = function (incident) {
+        nexioChat.prototype.socket.emit('notify', incident);
+        // the server should:
+        // io.to(incident).emit('onmessage');
+    };
+
+    nexioChat.prototype.listen = function () {
+        nexioChat.prototype.socket.on('onmessage', function (incident) {
+            console.log("received message from: " + incident);
+            // here comes the js controller to refresh the chat view.
+            nexioChat.prototype.callback(incident);
+        });
+    }
+
+    nexioChat.prototype.init = function (options) {
+        nexioChat.prototype.socket = options.url === undefined ? io() : io(options.url);
+        nexioChat.prototype.callback = options.callback;
+        nexioChat.prototype.listen();
+        nexioChat.prototype.join(options.incidents)
+    };
+
+    nexioChat.prototype.init(options);
+
+}
